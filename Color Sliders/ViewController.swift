d0639cf6e911dc494bb9e6d3e9a66cdc0e30a0da
Jacob Hendrich <hendrijm@rose-hulman.edu>
2020-12-03 14:16:59 -0500

    First push

---
+++ Color Sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color Sliders
+//
+//  Created by Jacob Hendrich on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
